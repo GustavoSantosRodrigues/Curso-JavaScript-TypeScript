54744a9086bcb34cf1ea6343baacc2b7dae09ab5
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2022-11-07 07:47:11 -0300

    Ensinamentos de como usar alert, prompt e confirm, efetuando uma conta pedindo para usuario digitar um numero e depois transformando em numero e somando e retornando o resultado em alert

---
+++ aula-10 operadores/index.js
@@ -0,0 +1,29 @@
+/* ordem e incremento  e decremento 
+()
+**
+* / %
++ - 
+*/
+
+/*
+operadores de atribuicao
++=
+*/
+
+/*
+NaN not a number, ParsrInt(), ParseFloat, Number.
+*/
+const num1 = 10;
+const num2 = Number('5.10');
+console.log(num1 + num2);
+
+// const passo = 10;
+// let contador = 0;
+// contador += 10; // contador = contador + passo;
+// contador += 10;
+// contador += 10;
+// console.log(contador);
+
+
+
+
---
+++ aula-11 alert, confirm e cmd/index.js
@@ -0,0 +1,21 @@
+// alert('Mensagem para aparecer.');
+// window.confirm('teste');
+//window.prompt('mensagem');
+
+// let number 1 let number 2 
+// let result parecer soma
+    
+
+let number1 = prompt('Digite um numero!');
+let number2 = prompt('Digite outro um numero!');
+number1 = Number(number1);
+number2 = Number(number2);
+const result = number1 + number2;
+console.log(number1 + number2); 
+alert (`seu numero é: ${result}`);
+
+
+
+
+
+
