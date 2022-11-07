474c1e429bfa6e5978464ace6b2c5f6cec8f18b4
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2022-11-07 08:20:14 -0300

    Criando VarA, VarB, Varc e tem que aparecer B C A no terminal

---
+++ aula-12/index.js
@@ -0,0 +1,21 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+[varA, varB, varC] = [varB, varC, varA];  //uma maneira mais nova sem precisar criar uma variavel Temporaria
+console.log(varA, varB, varC);
+
+// let varA = 'A';
+// let varB = 'B';
+// let varC = 'C';
+// let armazenar = varA;
+
+// varA = varB;
+// varB = varC;
+// varC = armazenar;
+
+
+// console.log(varA, varB, varC);
+
+// varA = b
+// varB = c
+// varC = A
