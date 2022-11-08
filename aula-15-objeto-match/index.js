7fdb0615b714bfd8da0486ae189e072f65b8a7b1
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2022-11-08 09:07:38 -0300

    aula 15 objeto match

---
+++ aula-15-objeto-match/index.js
@@ -0,0 +1,10 @@
+let num1 = 25;
+
+// console.log(aleatorio); 
+// const aleatorio = Math.round(Math.random() * (40 -5) + 5);  serve para fazer um randon dentro de 40 ate -5 
+// num2= Math.min(1,3,4,700,-50,1000);
+// num2= Math.max(1,3,4,700,-50,1000);
+// num2 = Math.round(num1); arrendona para baixo, 7.4 arrendonda para 7 e 7.5 arredonda para cima
+// let num2 = Math.round(num1);  arrendona para cima 
+// let num2 = Math.ceil(num1); arrendona para cima com 8
+// let num2 = Math.floor(num1); arrendonda para baixo numero decimal de 7.987 para 7
