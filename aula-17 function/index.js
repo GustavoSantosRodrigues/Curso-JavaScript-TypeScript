56c4e842f4dee8c935f94cd60321b6de3b84ae69
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2022-11-14 18:31:40 -0300

    funcoes basicas em js

---
+++ aula-17 function/index.js
@@ -0,0 +1,25 @@
+// arrow function
+
+// const raiz = n => n ** 0.5
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+
+//funcao anonima
+
+// const raiz = function (n) {
+//     return n ** 0.5
+// };
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+
+// function soma (nota1 , nota2) {
+//     const resultado = nota1 + nota2;
+//     return resultado;
+// }
+
+// const resultado = soma(10, 10);
+// console.log(resultado);
