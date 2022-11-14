257fdb3d419ff26f17920ccabb44ed4b705902f0
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2022-11-14 19:07:55 -0300

    objetos basicos

--- aula-17 function/index.js
+++ aula-17 function/index.js
@@ -1,9 +1,9 @@
 // arrow function
 
-// const raiz = n => n ** 0.5
-// console.log(raiz(9));
-// console.log(raiz(16));
-// console.log(raiz(25));
+const raiz = n => n ** 0.5
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
 
 
 //funcao anonima
---
+++ aula-18 objetos/index.js
@@ -0,0 +1,16 @@
+function pessoa1 ()
+
+
+// function criaPessoa (nome, sobrenome, idade) {
+//     return { 
+//         nome, 
+//         sobrenome, 
+//         idade
+//     }
+// }
+
+// const pessoa1 = criaPessoa('Gustavo', 'Santos' , 20);
+// const pessoa2 = criaPessoa('Joana', 'Rodrigues' , 30);
+// const pessoa3 = criaPessoa('Leonardo', 'Oliveira' , 40);
+
+// console.log(pessoa1, pessoa2, pessoa3);
