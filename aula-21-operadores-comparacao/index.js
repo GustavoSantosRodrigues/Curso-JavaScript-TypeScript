b60996b97c339b6f534fe3b9efd7c8c0743a5f63
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2022-11-15 19:51:34 -0300

    operadores de comparacao > ou <. >= ou <= , === igualdade estrita ou !== diferente estrita os dois chega valor e tipo

---
+++ aula-21-operadores-comparacao/index.js
@@ -0,0 +1,19 @@
+/*
+operadores  de comparacao
+
+> maior que 
+>= maior igual
+< menor que
+<= menor igual
+
+=== igualdade estrita
+!== diferente estrita
+*/
+
+// const numer1 = 10;
+// const number2 = 10
+// const comp = numer1 === number2;
+// console.log(comp)
+
+// const expressao = 10 > 5;
+// console.log(expressao);
