d5b082759ed23576c192939f1bf4ee26011508e5
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2022-11-14 21:25:17 -0300

    tips primitivos

--- aula-18 objetos/index.js
+++ aula-18 objetos/index.js
@@ -1,16 +1,14 @@
-function pessoa1 ()
 
+function criaPessoa (nome, sobrenome, idade) {
+    return { 
+        nome, 
+        sobrenome, 
+        idade
+    }
+}
 
-// function criaPessoa (nome, sobrenome, idade) {
-//     return { 
-//         nome, 
-//         sobrenome, 
-//         idade
-//     }
-// }
+const pessoa1 = criaPessoa('Gustavo', 'Santos' , 20);
+const pessoa2 = criaPessoa('Joana', 'Rodrigues' , 30);
+const pessoa3 = criaPessoa('Leonardo', 'Oliveira' , 40);
 
-// const pessoa1 = criaPessoa('Gustavo', 'Santos' , 20);
-// const pessoa2 = criaPessoa('Joana', 'Rodrigues' , 30);
-// const pessoa3 = criaPessoa('Leonardo', 'Oliveira' , 40);
-
-// console.log(pessoa1, pessoa2, pessoa3);
+console.log(pessoa1, pessoa2, pessoa3);
---
+++ aula-19-valores-primitivos/index.js
@@ -0,0 +1,17 @@
+/*
+    primitivos (imutaveis) -string, number, boolean, undefined
+    null (bigint, symbol) valores copiados
+
+    let numero = 10;
+    numero = 20;
+
+    referencias (mutavel) array, object , function - passados por referencias
+   const a = {
+    nome: Luiz,
+    idade: 20
+   }
+
+   const b = a
+   a.nome = 'joao'
+   console.log(b); --> {joao}, {20}
+*/
