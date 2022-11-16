92167369b88c752dff06b7eef1eca265402d658e
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2022-11-15 21:23:16 -0300

    operadores logicos

---
+++ aula-22-operadores-logicos/index.js
@@ -0,0 +1,17 @@
+/*
+&& and E
+|| our ou 
+! not nao
+*/
+
+const usuario = 'luiz';
+const senha = 123456;
+
+// console.log(!true) retorna false 
+
+// const vaiLogar = usuario === 'luiz' || senha == 123456
+// console.log(vaiLogar)
+
+// const expressaoAnd = true && true; tem que ser true para retornar true
+// const expressaoOur = true || false;  tem que ser verdadeiro ou falso 
+// console.log(expressaoOur);
