b6819748ac0276a3bf20dafbb62dde15c77aa34f
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2023-02-07 21:02:46 -0300

    aprendeno sobre if else e else if

---
+++ aula-24-if-else/index.js
@@ -0,0 +1,32 @@
+/**
+entre 5-11 'bom dia'
+entre 12- 17 'boa tarde'
+entre 18-23 'boa noite' 
+entre 23 - 4 'boa madrugada
+
+ */
+
+// if pode ser usado sozinho
+// else sempre que utilizado a palavra else, preciso de umm if antes
+// pode ter varios else if na condição
+// so pode ter um else na condicao
+
+// const hora = 52;
+
+// if (hora >= 0 && hora <= 11) {
+//   console.log("BOM DIA");
+// } else if (hora >= 12 && hora <= 17) {
+//   console.log("BOA TARDE");
+// } else if (hora >= 18 && hora <= 23) {
+//   console.log("BOA NOITE");
+// }else {
+//     console.log('Bem Vindo!')
+// }
+
+const tenhoGrana = 110;
+
+if(tenhoGrana <=100){
+    console.log('posso sair')
+}else {
+    console.log('lascou preciso de grana')
+}
