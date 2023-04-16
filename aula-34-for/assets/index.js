769a42a113aceae4ead6e547d62405a4945109e1
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2023-04-16 17:58:21 -0300

    for js

---
+++ aula-34-for/assets/index.js
@@ -0,0 +1,9 @@
+const frutas = ['maca', 'banana', 'pera', 'uva'];
+for(let i = 0; i < frutas.length; i++){
+    console.log(`Indice ${i}`, frutas[i])
+}
+
+// for(let i = 0; i < 20; i++){
+//     const par = i % 2 === 0 ? 'par' : 'impar';
+//     console.log(i, par)
+// }
