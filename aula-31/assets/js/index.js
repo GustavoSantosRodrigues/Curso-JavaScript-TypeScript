4cb305f7913b75562d5131368a33c74031440465
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2023-04-16 15:32:55 -0300

    mais diferencas entre var, let/const

---
+++ aula-31/assets/js/index.js
@@ -0,0 +1,34 @@
+// let tem escopo de block {.... bloco}
+// var tem escopo de funcao
+
+const verdadeiro = true; 
+
+// let nome = 'LUIZ';
+// var nome2 = 'Luiz';
+
+// if(verdadeiro){
+//     let nome = 'joao';// criando 
+//     var nome2 = 'joana'; //redeclarando
+//     // console.log(nome, nome2)
+    
+//     if(verdadeiro){
+//         let nome = 'gustavo'; //criando
+//         var nome2 = 'matheus' //redeclarando
+//     } //aninhado   
+// }
+
+// console.log(nome, nome2);
+
+// function falaOi(){
+//     var sobrenome = "santos";
+    
+//     if(verdadeiro){
+//         let nome = "gustavo"
+//         var sobrenome2 = 'rodrigues'
+//         console.log(nome)
+//     }
+//     console.log(sobrenome, sobrenome2)
+// }
+// falaOi();
+
+
