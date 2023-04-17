69696259dbeb98ea1837724b26a00b78911ff4cd
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2023-04-16 22:01:11 -0300

    diferentes for, for in, for of

---
+++ aula-37-for-of/assets/index.js
@@ -0,0 +1,20 @@
+/*
+for classico: geralmente com iteraveis [array] ou 'string
+for in: retorna o indice ou chaves [array], {chaves} ou 'string'
+for o: retorna o valor em si [array] ou 'strings'
+*/
+
+
+// const nome = 'Gustavo Santos'
+
+// for(let valor of nome){
+//     console.log(valor)
+// }
+
+// for(let i in nome){
+//     console.log(nome[i])
+// }
+
+// for(i = 0; i < nome.length; i++){
+//     console.log(nome[i])
+// }
